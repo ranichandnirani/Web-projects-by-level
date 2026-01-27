4401824dc8551fab4127e91cbfbcc86bb2b3d6a4
ranichandnirani <chandnirani229@gmail.com>
2026-01-27 13:57:57 +0530

    add some styling and functionality

---
+++ To-Do List/app.js
@@ -0,0 +1,39 @@
+const btnEle = document.querySelector(".btn")
+const inputEle = document.querySelector("#input-field")
+const taskEle = document.querySelector(".list-item")
+
+btnEle.addEventListener("click", ()=> {
+    if(inputEle.value === "") {
+        alert("Please add your task first")
+    } else {
+        // create list
+
+        let listEle = document.createElement("li")
+        listEle.textContent = inputEle.value
+        taskEle.appendChild(listEle)
+
+        // for remove task when it's done
+        let spanEle = document.createElement("span")
+        spanEle.textContent = "\u00d7"
+        listEle.appendChild(spanEle)
+    }
+    inputEle.value = ""
+    saveEle
+})
+
+taskEle.addEventListener("click", (e) => {
+    if(e.target.tagName == "LI") {
+        e.target.classList.toggle("checked")
+    } else if(e.target.tagName == "SPAN") {
+        e.target.parentElement.remove()
+        saveEle
+    }
+})
+
+function saveEle() {
+    localStorage.setItem("task", taskEle.innerHTML)
+}
+
+function getEle() {
+    taskEle.innerHTML = localStorage.getItem("task")
+}
