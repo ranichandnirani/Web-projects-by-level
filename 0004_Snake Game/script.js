825e7088ac9ff82639ac662aa65a14b994bb6b8c
Chandni Rani <chandnirani229@gmail.com>
2026-03-11 18:36:05 +0530

    snake movement on board

--- 0004_Snake Game/script.js
+++ 0004_Snake Game/script.js
@@ -1,11 +1,15 @@
 // for selecting board
 
 const board = document.querySelector('.board');
-const blockHeight = 30
-const blockWidth = 30
+const blockHeight = 50
+const blockWidth = 50
 
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
+
+const blocks = [];
+const snake = [{r: 1, c: 3}]
+let direction = "right"
 
 // creating bolcks
 
@@ -20,5 +24,38 @@ for(let row = 0; row < rows; row++) {
         const block = document.createElement("div");
         block.classList.add("block");
         board.appendChild(block);
+        block.innerText = `${row}-${col}`;
+        blocks[ `${row}-${col}` ] = block;
     }
 }
+
+function render(){
+    snake.forEach(segment =>{
+        // console.log(segment)
+        blocks[`${segment.r}-${segment.c}`].classList.add("fill")
+    });
+}
+
+render();
+
+setInterval(() => {
+
+    let head = null;
+
+    if(direction === "left") {
+        head = {r: snake[ 0 ].r, c: snake[ 0 ].c-1}
+    } else if(direction === "right") {
+        head = {r: snake[ 0 ].r, c: snake[ 0 ].c+1}
+    }
+
+
+    snake.forEach(segment => {
+        blocks[`${segment.r}-${segment.c}`].classList.remove("fill");
+    });
+
+    snake.unshift(head);
+    snake.pop();
+
+    render();
+
+}, 400);
