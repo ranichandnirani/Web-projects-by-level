ac398a1762f0a0b903fc4796454f345c0e22d63c
Chandni Rani <chandnirani229@gmail.com>
2026-03-11 16:40:02 +0530

    some changes

--- 0004_Snake Game/script.js
+++ 0004_Snake Game/script.js
@@ -1,8 +1,8 @@
 // for selecting board
 
 const board = document.querySelector('.board');
-const blockHeight = 80
-const blockWidth = 80
+const blockHeight = 30
+const blockWidth = 30
 
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
