9c27cda2da828d3fb0ca8b36f114f9593d7515ff
Chandni Rani <chandnirani229@gmail.com>
2026-03-15 22:05:49 +0530

    Snake game is completed

--- 0004_Snake Game/script.js
+++ 0004_Snake Game/script.js
@@ -3,36 +3,41 @@
 const board = document.querySelector(".board");
 const startButton = document.querySelector(".btn-start");
 const modal = document.querySelector(".modal");
-const startGameModal = document.querySelector(".game-start");
+const startGameModal = document.querySelector(".start-game");
 const gameOverModal = document.querySelector(".game-over");
+const restartButton = document.querySelector(".btn-restart");
+
+const highScoreElement = document.querySelector("#high-score");
+const scoreElement = document.querySelector("#score");
+const timeElement = document.querySelector("#time");
 
 const blockHeight = 50
 const blockWidth = 50
+
+let highScore = localStorage.getItem("highScore") || 0;
+let score = 0;
+let time = `00-00`;
+
+highScoreElement.innerText = highScore;
 
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
 
 let intervalId = null;
+let timmerIntervalId = null;
 let food = {r:Math.floor(Math.random()* rows), c:Math.floor(Math.random()*cols)}
 
 const blocks = [];
-const snake = [{r: 1, c: 3}]
+let snake = [{r: 1, c: 3}]
 let direction = "right"
 
 // creating bolcks
-
-// for(let i = 0; i < rows*cols; i++) {
-//     const block = document.createElement("div");
-//     block.classList.add("block");
-//     board.appendChild(block);
-// }
 
 for(let row = 0; row < rows; row++) {
     for(let col = 0; col < cols; col++) {
         const block = document.createElement("div");
         block.classList.add("block");
         board.appendChild(block);
-        block.innerText = `${row}-${col}`;
         blocks[ `${row}-${col}` ] = block;
     }
 }
@@ -55,11 +60,13 @@ function render(){
 
 
     // game over alert
-    if(head.r < 0 || head.r > rows || head.c < 0 || head.c >cols) {
+    if(head.r < 0 || head.r >= rows || head.c < 0 || head.c >= cols) {
         // alert("Game Over");
         clearInterval(intervalId);
 
         modal.style.display = "flex";
+        gameOverModal.style.display = "flex";
+        startGameModal.style.display = "none";
         
         return;
     }
@@ -72,6 +79,16 @@ function render(){
 
         // for increasing tail after eating food
         snake.unshift(head);
+
+        // increase score when snake eat food.
+        score += 10;
+        scoreElement.innerText = score;
+
+        // Store score in high score
+        if(score > highScore) {
+            highScore = score;
+            localStorage.setItem("highScore", highScore.toString());
+        }
     }
 
     snake.forEach(segment => {
@@ -88,24 +105,65 @@ function render(){
     });
 }
 
-// render();
-
-intervalId = setInterval(() => {
-
-
-    render();
-
-}, 400);
-
 startButton.addEventListener("click", () =>{
-    // modal.Style.display = "none";
-    modal.remove();
+    modal.style.display = "none";
+    if (intervalId !== null) {
+        clearInterval(intervalId);
+    }
     intervalId = setInterval(()=> {render()}, 300);
+    timmerIntervalId = setInterval(()=> {
+
+      let [min, sec] = time.split("-").map(Number);
+
+      if(sec == 59) {
+        min += 1;
+        sec = 0;
+      } else {
+        sec += 1;
+      }
+
+      time = `${min}-${sec}`;
+      timeElement.innerText = time;
+    }, 1000);
 });
+
+// Restart game
+restartButton.addEventListener("click", restarGame);
 
 function restarGame() {
 
+    blocks[`${food.r}-${food.c}`].classList.add("food");
+    snake.forEach(segment => {
+        blocks[`${segment.r}-${segment.c}`].classList.remove("fill");
+    });
+
+    // score after restart the game
+    score = 0;
+    time = `00-00`;
+
+    scoreElement.innerText = score;
+    timeElement.innerText = time;
+    highScoreElement.innerText = highScore;
+
+
+    modal.style.display = "none";
+    gameOverModal.style.display = "none";
+    startGameModal.style.display = "none";
+    if (intervalId !== null) {
+        clearInterval(intervalId);
+    }
+    snake.forEach(segment => {
+        blocks[`${segment.r}-${segment.c}`]?.classList.remove("fill");
+    });
+    blocks[`${food.r}-${food.c}`]?.classList.remove("food");
+    snake = [{r: 1, c: 3}];
+    food = {r:Math.floor(Math.random()* rows), c:Math.floor(Math.random()*cols)}
+    direction = "right";
+    intervalId = setInterval(()=> {render()}, 300);
+
 }
+
+// Movement
 // ArrowUp
 // ArrowRight
 // ArrowLeft
