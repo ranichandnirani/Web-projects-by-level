c3d0e14cf47e2f30e096f4a972cb6b76d162fdc5
Chandni Rani <chandnirani229@gmail.com>
2026-04-23 23:26:32 +0530

    done

--- 0006_Food Delivery/main.js
+++ 0006_Food Delivery/main.js
@@ -1,5 +1,3 @@
-const { createElement } = require("react");
-
 var swiper = new Swiper(".mySwiper", {
   loop: true,
   navigation: {
@@ -13,27 +11,65 @@ const cartTab = document.querySelector(".cart-tab");
 const closeBtn = document.querySelector(".close-btn");
 const cardList = document.querySelector(".card-list");
 const cartList = document.querySelector(".cart-list");
+const cartTotal = document.querySelector(".cart-total");
+const cartValue = document.querySelector(".cart-value");
+const hamburger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".mobile-menu");
+const bars = document.querySelector(".fa-bars");
+const mobileLinks = document.querySelectorAll(".mobile-menu a");
 
 // Active and close add cart section
-cartIcon.addEventListener("click", () => {
-  cartTab.classList.add("cart-tab-active");
+cartIcon.addEventListener("click", () => cartTab.classList.add("cart-tab-active"));
+closeBtn.addEventListener("click", () => cartTab.classList.remove("cart-tab-active"));
+hamburger.addEventListener("click", (e) => {
+  e.preventDefault();
+  mobileMenu.classList.toggle("mobile-menu-active");
+});
+hamburger.addEventListener("click", (e) => {
+  e.preventDefault();
+  bars.classList.toggle("fa-bars");
+  bars.classList.toggle("fa-xmark");
 });
 
-closeBtn.addEventListener("click", () => {
-  cartTab.classList.remove("cart-tab-active");
+mobileLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.remove("mobile-menu-active");
+    bars.classList.add("fa-bars");
+    bars.classList.remove("fa-xmark");
+  });
 });
+
+let productList = [];
+let cartProduct = [];
+
+// Total Price
+const updateTotals = () => {
+
+  let totalPrice = 0;
+  let totalQuantity = 0;
+
+  document.querySelectorAll(".item").forEach(item => {
+
+    const quantity = parseInt(item.querySelector(".quantity-value").textContent);
+    const price = parseFloat(item.querySelector(".item-total").textContent.replace("$", ""));
+
+    totalPrice += price;
+    totalQuantity += quantity;
+  });
+
+  cartTotal.textContent = `$${totalPrice.toFixed(2)}`;
+  cartValue.textContent = totalQuantity
+};
 
 // Product list
-let productList = [];
-
 const showCards = () => {
   productList.forEach(product => {
-    const orderCard = document.createElement('div');
+    const orderCard = document.createElement("div");
     orderCard.classList.add("order-card");
 
     orderCard.innerHTML = `
     <div class="card-image">
-      <img src="${product.image}" alt="">
+      <img src="${product.image}" alt="${product.name}">
     </div>
     <h4>${product.name}</h4>
     <h4 class="price">${product.price}</h4>
@@ -41,39 +77,91 @@ const showCards = () => {
     `;
     cardList.appendChild(orderCard);
 
-    const cardBtn = orderCard.querySelector('.card-btn');
+    const cardBtn = orderCard.querySelector(".card-btn");
 
-    cardBtn.addEventListener('click', (e) => {
+    cardBtn.addEventListener("click", (e) => {
       e.preventDefault();
-      addToCart();
+      addToCart(product);
     });
   });
 };
 
-function addToCart() {
-  const cartItem = document.createElement('div');
+const addToCart = (product) => {
+
+  const existingProduct = cartProduct.find(item => item.id === product.id);
+
+  if(existingProduct) {
+    alert("Already is your cart")
+    return;
+
+  }
+
+  cartProduct.push(product);
+  let quantity = 1;
+  let price = parseFloat(product.price.replace("$", ""))
+
+  const cartItem = document.createElement("div");
   cartItem.classList.add("item");
 
   cartItem.innerHTML = `
     <div class="item-image">
-      <img src="Images/burger.png" alt="">
+      <img src="${product.image}" alt="${product.name}">
     </div>
-    <div>
-      <h4>Double Beef Burger</h4>
-      <h4 class="item-total">$200</h4>
+    <div class="detail">
+      <h4>${product.name}</h4>
+      <h4 class="item-total">${product.price}</h4>
     </div>
     <div class="flex">
-      <a href="#" class="quantity-btn">
+      <a href="#" class="quantity-btn minus">
         <i class="fa-solid fa-minus"></i>
       </a>
-      <h4 class="quantity-value">1</h4>
-      <a href="#" class="quantity-btn">
+      <h4 class="quantity-value">${quantity}</h4>
+      <a href="#" class="quantity-btn plus">
         <i class="fa-solid fa-plus"></i>
       </a>
     </div>
   `;
   cartList.appendChild(cartItem);
-}
+  updateTotals();
+
+  const plusBtn = cartItem.querySelector(".plus");
+  const quantityValue = cartItem.querySelector(".quantity-value");
+  const itemTotal = cartItem.querySelector(".item-total");
+  const minusBtn = cartItem.querySelector(".minus");
+
+  plusBtn.addEventListener("click", (e) => {
+
+    e.preventDefault();
+
+    quantity++;
+    quantityValue.textContent = quantity;
+    itemTotal.textContent = `$${(price * quantity).toFixed(2)}`;
+    updateTotals();
+
+  });
+  minusBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if(quantity > 1) {
+      quantity--;
+      quantityValue.textContent = quantity;
+      itemTotal.textContent = `$${(price * quantity).toFixed(2)}`;
+      updateTotals();
+
+    }
+    else {
+      cartItem.classList.add("slide-out");
+      
+      setTimeout(() => {
+        cartItem.remove();
+        cartProduct = cartProduct.filter(item => item.id !== product.id);
+        updateTotals();
+
+      }, 300)
+    }
+    
+  })
+};
 
 const initApp = () => {
   fetch("products.json")
